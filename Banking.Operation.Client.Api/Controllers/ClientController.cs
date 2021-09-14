e45070eeca1403aa5a7b5c0174038ecf585bb615
Edson Caliman <edsonjunior@db1.com.br>
2021-09-14 17:28:24 -0300

    Solution, folders and initial projects

---
+++ Banking.Operation.Client.Api/Controllers/ClientController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Banking.Operation.Client.Api.Controllers
+{
+    public class ClientController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
