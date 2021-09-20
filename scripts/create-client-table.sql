0ce3338b8ad06193dddd90cf853aed6c6c0a4fda
Edson Caliman <edsonjunior@db1.com.br>
2021-09-20 13:54:50 -0300

    Add contact table

--- scripts/create-client-table.sql
+++ scripts/create-client-table.sql
@@ -7,3 +7,14 @@ CREATE TABLE `Clients` (
   `CreatedBy` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
   PRIMARY KEY (`Id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+
+CREATE TABLE `Contacts` (
+  `Id` char(36) CHARACTER SET ascii COLLATE ascii_general_ci NOT NULL,
+  `Name` varchar(150) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `ClientId` char(36) CHARACTER SET ascii COLLATE ascii_general_ci DEFAULT NULL,
+  `CreatedAt` datetime(6) NOT NULL,
+  `CreatedBy` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
+  PRIMARY KEY (`Id`),
+  KEY `IX_Contacts_ClientId` (`ClientId`),
+  CONSTRAINT `FK_Contacts_Clients_ClientId` FOREIGN KEY (`ClientId`) REFERENCES `Clients` (`Id`) ON DELETE RESTRICT
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
