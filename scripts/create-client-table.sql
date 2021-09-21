f62707807b7ccf35785c8439d365ff26f10a4470
Edson Caliman <edsonjunior@db1.com.br>
2021-09-21 17:49:20 -0300

    Add account property

--- scripts/create-client-table.sql
+++ scripts/create-client-table.sql
@@ -12,6 +12,7 @@ CREATE TABLE `Contacts` (
   `Id` char(36) CHARACTER SET ascii COLLATE ascii_general_ci NOT NULL,
   `Name` varchar(150) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
   `ClientId` char(36) CHARACTER SET ascii COLLATE ascii_general_ci DEFAULT NULL,
+  `Account` int NOT NULL,
   `CreatedAt` datetime(6) NOT NULL,
   `CreatedBy` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
   PRIMARY KEY (`Id`),
